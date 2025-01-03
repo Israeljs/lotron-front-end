e43cfb3fba6ee49bab61a69233b0a8757acf9ffd
israel jeronimo <silvaij16@gmail.com>
2025-01-02 21:38:29 -0300

    feature: header.js

---
+++ src/header.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var myLinks = document.getElementById("myLinks");
+  if (myLinks.style.display === "block") {
+    myLinks.style.display = "none";
+  } else {
+    myLinks.style.display = "block";
+  }
+}
