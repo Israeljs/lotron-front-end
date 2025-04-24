598db22c921a98942b5ea6d05fdb7350b16e2ed6
israel jeronimo <silvaij16@gmail.com>
2025-04-23 22:46:14 -0300

    feature: implementação do fetch

--- src/services/gamesFetch.js
+++ src/services/gamesFetch.js
@@ -1,7 +1,9 @@
+const fetch = require('node-fetch')
+
 module.exports = {
   gamesFetch: async () => {
     const response = await fetch('http://localhost:3000/game/10');
-    const data = await response.json();
+    const data = response.json();
     return data
   }
 }
